167f5a6432f26819f9c2a07e2f7649ec2b0b25bd
lokezinck <104770977+lokezinck@users.noreply.github.com>
2022-05-12 16:40:52 +0200

    hade made it into a mudule

---
+++ image-preview/src/main/java/com/plcoding/image_preview/languagecenterliabary_features/data/repostory/ApiRepository.kt
@@ -0,0 +1,48 @@
+package com.example.languagecenterliabary.languagecenterliabary_features.data.repostory
+
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.example.languagecenterliabary.languagecenterliabary_features.data.remote.Api
+import com.example.languagecenterliabary.languagecenterliabary_features.domain.DaoModel
+import com.example.languagecenterliabary.languagecenterliabary_features.domain.api_models.PostStringModel
+import com.example.languagecenterliabary.languagecenterliabary_features.presentation.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
+import javax.inject.Inject
+
+class ApiRepository @Inject constructor(
+    private val api: Api
+) {
+
+    fun postString (
+        platform: String,
+        category: String,
+        key: String,
+        value: String,
+        comment: String,
+    ) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val data = api.postString(PostStringModel(platform, category, key, value, comment))
+                data
+                Log.d("dataaaaaaaa", "${data}")
+            } catch (e: IOException) {
+                Log.d("MainActivity", "${e}")
+            }
+        }
+    }
+
+    fun getAccountInfo() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val data = api.accountInfo()
+                data
+                Log.d("dataaaaaaaa", "${data}")
+            } catch (e: IOException) {
+                Log.d("MainActivity", "${e}")
+            }
+        }
+    }
+}
