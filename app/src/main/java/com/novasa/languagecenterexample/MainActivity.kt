e22fb2e938b8a89744e412c558950fb7d0e1bbd4
Mikkel Schläger <mikkel@novasa.com>
2022-05-16 15:50:20 +0200

    changed view model injection to correct syntax

--- app/src/main/java/com/novasa/languagecenterexample/MainActivity.kt
+++ app/src/main/java/com/novasa/languagecenterexample/MainActivity.kt
@@ -3,9 +3,11 @@ package com.novasa.languagecenterexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.novasa.languagecenter.languagecenterliabary_features.presentation.LCViewModel
 import com.novasa.languagecenterexample.ui.theme.AndroidLibraryTheme
 import com.novasa.languagecenter.languagecenterliabary_features.presentation.TreanslateableText
@@ -15,8 +17,7 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var viewModel: LCViewModel
+    private val viewModel: LCViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
