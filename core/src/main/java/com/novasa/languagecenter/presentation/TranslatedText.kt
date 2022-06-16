7be9f63be4ef839422350a050009927d2e1bb615
lokezinck <104770977+lokezinck@users.noreply.github.com>
2022-06-16 11:09:28 +0200

    hiltViewModel

--- core/src/main/java/com/novasa/languagecenter/presentation/TranslatedText.kt
+++ core/src/main/java/com/novasa/languagecenter/presentation/TranslatedText.kt
@@ -3,11 +3,12 @@ package com.novasa.languagecenter.presentation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun translatedText(
-    viewModel: LCViewModel = viewModel(),
+    viewModel: LCViewModel = hiltViewModel(),
     category: String,
     key: String,
     fallback: String,
